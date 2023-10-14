663135093ef609bd9693cf3bd44e10df8ef76928
augurybot3 <augurybot@gmail.com>
2023-10-14 13:38:42 -0400

    .

--- googleocr.py
+++ googleocr.py
@@ -42,177 +42,177 @@ def check_password():
         if st.session_state.private_key == False:
             st.stop()
         return True
-check_password()
+if check_password():
 
-print("****1. Set up and configure the SDK:****\nCredentials: ", config.cloud_name, config.api_key, "\n")
-
-api_secret = os.getenv("api_secret")
-
-def extract_id_information(result):
-    ocr_text = result['info']['ocr']['adv_ocr']['data'][0]['textAnnotations'][0]['description']
+    print("****1. Set up and configure the SDK:****\nCredentials: ", config.cloud_name, config.api_key, "\n")
     
-    patterns = {
-        "Full Name": r"MOTORIST\n([A-Z\s,]+)",
-        "Date of Birth": r"DOB (\d{2}/\d{2}/\d{4})",
-        "Address": r"([\w\s]+)\nYOUR CITY, NY",
-        "ID Number": r"ID (\d{3} \d{3} \d{3})",
-        "Issue Date": r"Issued (\d{2}/\d{2}/\d{4})",
-        "Expiry Date": r"Expires (\d{2}/\d{2}/\d{4})",
-        "Gender": r"Sex (\w)",
-        "Height": r"Height ([\d\'\-]+)"
-    }
-
-    extracted_data = {}
-    for field, pattern in patterns.items():
-        match = re.search(pattern, ocr_text)
-        extracted_data[field] = match.group(1).strip() if match else None
-
-    address_parts = extracted_data["Address"].split("\n")
-    if len(address_parts) > 1:
-        refined_address = address_parts[-1]
-    else:
-        refined_address = extracted_data["Address"]
-    extracted_data["Address"] = refined_address
-
-    return extracted_data
-
-def data_form_callback():
-    st.session_state.data_form_occurrence = True 
-def extraction_callback():
-    st.session_state.extraction_occurrence = True
-def final_callback():
-    st.session_state.start_occurrence = False
-    st.session_state.upload_occurrence = False
-
-if "start_occurrence" not in st.session_state:
-    st.session_state.start_occurrence = False
-if "upload_occurrence" not in st.session_state:
-    st.session_state.upload_occurrence = False
-if "extraction_occurrence" not in st.session_state:
-    st.session_state.extraction_occurrence = False
-if "data_form_occurrence" not in st.session_state:
-    st.session_state.data_form_occurrence = False
-if "csv" not in st.session_state:
-    st.session_state.csv = None
-if "excel" not in st.session_state:
-    st.session_state.excel = None
-if "json" not in st.session_state:
-    st.session_state.json = None
-
-st.title("ID Info Extraction Demo")
-
-if st.button("Upload Example Id"):
-
-    example_image = "example_id.jpg"
-    temp_path = "temp_path.jpg"
-    image = Image.open('example_id.jpg')
-    image.save(temp_path)
+    api_secret = os.getenv("api_secret")
     
-    if "example_image" not in st.session_state:
-        st.session_state.example_image = example_image
-    extracted_data = cloudinary.uploader.upload("example_id.jpg", ocr = "adv_ocr")
-    if "extracted_data" not in st.session_state:
-        st.session_state.extracted_data = extracted_data
-    full_json_output = json.dumps(extracted_data, indent=4)
-    if "json_output" not in st.session_state:
-        st.session_state.json_output = full_json_output
-
-    parsed_data = extract_id_information(st.session_state.extracted_data)
-    if "parsed_data" not in st.session_state:
-        st.session_state.parsed_data = parsed_data
-    df = pd.DataFrame([parsed_data])
-    if "df" not in st.session_state:
-        st.session_state.df = df
-    csv = st.session_state.df.to_csv(index=False)
+    def extract_id_information(result):
+        ocr_text = result['info']['ocr']['adv_ocr']['data'][0]['textAnnotations'][0]['description']
+        
+        patterns = {
+            "Full Name": r"MOTORIST\n([A-Z\s,]+)",
+            "Date of Birth": r"DOB (\d{2}/\d{2}/\d{4})",
+            "Address": r"([\w\s]+)\nYOUR CITY, NY",
+            "ID Number": r"ID (\d{3} \d{3} \d{3})",
+            "Issue Date": r"Issued (\d{2}/\d{2}/\d{4})",
+            "Expiry Date": r"Expires (\d{2}/\d{2}/\d{4})",
+            "Gender": r"Sex (\w)",
+            "Height": r"Height ([\d\'\-]+)"
+        }
+    
+        extracted_data = {}
+        for field, pattern in patterns.items():
+            match = re.search(pattern, ocr_text)
+            extracted_data[field] = match.group(1).strip() if match else None
+    
+        address_parts = extracted_data["Address"].split("\n")
+        if len(address_parts) > 1:
+            refined_address = address_parts[-1]
+        else:
+            refined_address = extracted_data["Address"]
+        extracted_data["Address"] = refined_address
+    
+        return extracted_data
+    
+    def data_form_callback():
+        st.session_state.data_form_occurrence = True 
+    def extraction_callback():
+        st.session_state.extraction_occurrence = True
+    def final_callback():
+        st.session_state.start_occurrence = False
+        st.session_state.upload_occurrence = False
+    
+    if "start_occurrence" not in st.session_state:
+        st.session_state.start_occurrence = False
+    if "upload_occurrence" not in st.session_state:
+        st.session_state.upload_occurrence = False
+    if "extraction_occurrence" not in st.session_state:
+        st.session_state.extraction_occurrence = False
+    if "data_form_occurrence" not in st.session_state:
+        st.session_state.data_form_occurrence = False
     if "csv" not in st.session_state:
-        st.session_state.csv = csv
-    excel = st.session_state.df.to_excel('id_data.xlsx', index=False)
+        st.session_state.csv = None
     if "excel" not in st.session_state:
-            st.session_state.excel = excel
-    output_json = json.dumps(parsed_data, indent=4)
-            
-    st.image(st.session_state.example_image, caption="uploaded identification", use_column_width=True)
-    st.session_state.upload_occurrence = True
-    os.remove(temp_path)
- 
-if st.session_state.extraction_occurrence == False and st.session_state.upload_occurrence == True:
-    with st.form("extract_form"):
-        
-        extraction_occurrence = st.form_submit_button("Extract", on_click=extraction_callback)
-        
-if st.session_state.extraction_occurrence == True and st.session_state.data_form_occurrence == False:
-    with st.form("data_form"):
-        st.sidebar.image(st.session_state.example_image, caption="uploaded ID",width=None)
-        
-        st.markdown("""please look over this form and update any differences or errors. 
-                    \n### ***!IMPORTANT!*** 
-                    \n***your information must match the information on your ID*** \n---\n*
-                    \nplease ensure this information matches exactly with the information shown on your ID.*""")
-        
-        edited_data = st.data_editor(st.session_state.df,  hide_index=True)
-        if "edited_data" not in st.session_state:
-            st.session_state.edited_data = edited_data
-            
-        submit_button = st.form_submit_button("Confirm and Submit", on_click=data_form_callback)
+        st.session_state.excel = None
+    if "json" not in st.session_state:
+        st.session_state.json = None
     
-
-if st.session_state.extraction_occurrence == True and st.session_state.data_form_occurrence == True:
-    st.markdown("""## **Thanks For Submitting Your Info For Testing!**\n---\n""")
-    st.markdown("""### You can download a copy of your data as a JSON file, CSV file, or Excel file.\n""")
-    space = st.empty()
-    with space.container():
-        st.write("    ")
+    st.title("ID Info Extraction Demo")
     
-    col1, col2, col4 = st.columns([1,1,1])
-    with col2:
-        with st.container():
-            placeholder1 = st.empty()
-        with st.container():
-            placeholder2 = st.empty()
-        with st.container():
-            placeholder3 = st.empty()
-        with st.container():
-            placeholder4 = st.empty()
-
-    with col1:        
-
-        colsb1 = st.empty()
-        cb1 = colsb1.checkbox("csv")
-        colsb2 = st.empty()
-        cb2 = colsb2.checkbox("excel")
-        colsb3 = st.empty()
-        cb3 = colsb3.checkbox("json")
-
-    with col4:
-        with st.container():
-            holder1 = st.empty()
-            holder2 = st.empty()
-            holder3 = st.empty()
-            holder4 = st.empty()
-        if cb1:
-            placeholder1.download_button(
-                label = "csv download",
-                data = "st.session_state.csv",
-                file_name = "id_info.csv",
-                mime="text/csv", key="download_button",
-            )
-        if cb2:
-            placeholder2.download_button(
-                label = "excel download",
-                data = "st.session_state.excel",
-                file_name = "id_info.xlsx",
-                mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", key="download_button2",
-            )
-        if cb3:
-            placeholder3.download_button(
-                label = "json download",
-                data = "st.session_state.output_json",
-                file_name = "id_info.json",
-                mime = "application/json", key="download_button3",
-            )
-
-    reset_button = placeholder4.button("RESET DEMO", on_click=final_callback)
-    if reset_button:
-        for key in st.session_state.keys():
-            del st.session_state[key]
-        st.rerun()
+    if st.button("Upload Example Id"):
+    
+        example_image = "example_id.jpg"
+        temp_path = "temp_path.jpg"
+        image = Image.open('example_id.jpg')
+        image.save(temp_path)
+        
+        if "example_image" not in st.session_state:
+            st.session_state.example_image = example_image
+        extracted_data = cloudinary.uploader.upload("example_id.jpg", ocr = "adv_ocr")
+        if "extracted_data" not in st.session_state:
+            st.session_state.extracted_data = extracted_data
+        full_json_output = json.dumps(extracted_data, indent=4)
+        if "json_output" not in st.session_state:
+            st.session_state.json_output = full_json_output
+    
+        parsed_data = extract_id_information(st.session_state.extracted_data)
+        if "parsed_data" not in st.session_state:
+            st.session_state.parsed_data = parsed_data
+        df = pd.DataFrame([parsed_data])
+        if "df" not in st.session_state:
+            st.session_state.df = df
+        csv = st.session_state.df.to_csv(index=False)
+        if "csv" not in st.session_state:
+            st.session_state.csv = csv
+        excel = st.session_state.df.to_excel('id_data.xlsx', index=False)
+        if "excel" not in st.session_state:
+                st.session_state.excel = excel
+        output_json = json.dumps(parsed_data, indent=4)
+                
+        st.image(st.session_state.example_image, caption="uploaded identification", use_column_width=True)
+        st.session_state.upload_occurrence = True
+        os.remove(temp_path)
+    
+    if st.session_state.extraction_occurrence == False and st.session_state.upload_occurrence == True:
+        with st.form("extract_form"):
+            
+            extraction_occurrence = st.form_submit_button("Extract", on_click=extraction_callback)
+            
+    if st.session_state.extraction_occurrence == True and st.session_state.data_form_occurrence == False:
+        with st.form("data_form"):
+            st.sidebar.image(st.session_state.example_image, caption="uploaded ID",width=None)
+            
+            st.markdown("""please look over this form and update any differences or errors. 
+                        \n### ***!IMPORTANT!*** 
+                        \n***your information must match the information on your ID*** \n---\n*
+                        \nplease ensure this information matches exactly with the information shown on your ID.*""")
+            
+            edited_data = st.data_editor(st.session_state.df,  hide_index=True)
+            if "edited_data" not in st.session_state:
+                st.session_state.edited_data = edited_data
+                
+            submit_button = st.form_submit_button("Confirm and Submit", on_click=data_form_callback)
+        
+    
+    if st.session_state.extraction_occurrence == True and st.session_state.data_form_occurrence == True:
+        st.markdown("""## **Thanks For Submitting Your Info For Testing!**\n---\n""")
+        st.markdown("""### You can download a copy of your data as a JSON file, CSV file, or Excel file.\n""")
+        space = st.empty()
+        with space.container():
+            st.write("    ")
+        
+        col1, col2, col4 = st.columns([1,1,1])
+        with col2:
+            with st.container():
+                placeholder1 = st.empty()
+            with st.container():
+                placeholder2 = st.empty()
+            with st.container():
+                placeholder3 = st.empty()
+            with st.container():
+                placeholder4 = st.empty()
+    
+        with col1:        
+        
+            colsb1 = st.empty()
+            cb1 = colsb1.checkbox("csv")
+            colsb2 = st.empty()
+            cb2 = colsb2.checkbox("excel")
+            colsb3 = st.empty()
+            cb3 = colsb3.checkbox("json")
+    
+        with col4:
+            with st.container():
+                holder1 = st.empty()
+                holder2 = st.empty()
+                holder3 = st.empty()
+                holder4 = st.empty()
+            if cb1:
+                placeholder1.download_button(
+                    label = "csv download",
+                    data = "st.session_state.csv",
+                    file_name = "id_info.csv",
+                    mime="text/csv", key="download_button",
+                )
+            if cb2:
+                placeholder2.download_button(
+                    label = "excel download",
+                    data = "st.session_state.excel",
+                    file_name = "id_info.xlsx",
+                    mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", key="download_button2",
+                )
+            if cb3:
+                placeholder3.download_button(
+                    label = "json download",
+                    data = "st.session_state.output_json",
+                    file_name = "id_info.json",
+                    mime = "application/json", key="download_button3",
+                )
+    
+        reset_button = placeholder4.button("RESET DEMO", on_click=final_callback)
+        if reset_button:
+            for key in st.session_state.keys():
+                del st.session_state[key]
+            st.rerun()
--- pytesseract.py
+++ pytesseract.py
@@ -35,102 +35,102 @@ def check_password():
         if st.session_state.private_key == False:
             st.stop()
         return True
-check_password()
-
-extracted_text = get_text_extraction()
-
-if "extracted_text" not in st.session_state:
-    st.session_state.extracted_text = extracted_text
-if "upload_occurrence" not in st.session_state:
-    st.session_state.upload_occurrence = False
-if "extraction_occurrence" not in st.session_state:
-    st.session_state.extraction_occurrence = False
-if "data_form_occurrence" not in st.session_state:
-    st.session_state.data_form_occurrence = False
-
-def data_form_callback():
-    st.session_state.data_form_occurrence = True
+if check_password():
     
-def extraction_callback():
-    st.session_state.extraction_occurrence = True
-
-def final_callback():
-    st.session_state.data_form_occurrence = False
-    st.session_state.extraction_occurrence = False
+    extracted_text = get_text_extraction()
     
-def parse_with_reg_expressions(text):
-    data = {}
-    name_match = re.search(r"MOTORIST\.\s*(?P<name>[A-Z\s,\.]+)\s*SAMPLE", text)
-    if name_match:
-        data["Full Name"] = name_match.group("name").replace(",", "").strip()
-    address_match = re.search(r"(?P<address>\d+\s[A-Z\s]+)\s*,\s*NY\s*\d+", text)
-    if address_match:
-        data["Address"] = address_match.group("address").strip()
-    sex_match = re.search(r"Sex\s*(?P<sex>[MF])", text)
-    if sex_match:
-        data["Sex"] = sex_match.group("sex")
-    height_match = re.search(r"Height\s*(?P<height>\d+'-\d+\")", text)
-    if height_match:
-        data["Height"] = height_match.group("height")
-    dob_match = re.search(r"(?P<dob>\d{2}/\d{2}/\d{4})", text)
-    if dob_match:
-        data["DOB"] = dob_match.group("dob")
-    exp_match = re.search(r"Exes\s*(?P<exp>\d{2}/\d{2}/\d{4})", text)
-    if exp_match:
-        data["Expiration Date"] = exp_match.group("exp")
-
-    issue_match = re.search(r"issued\s*(?P<issue>\d{2}/\d{2}/\d{4})", text)
-    if issue_match:
-        data["Issue Date"] = issue_match.group("issue")
-
-    return data
-
-st.title("ID Info Extraction Demo")
-
-example_image_path = "Image-to-Text-Scraper-Demo/example_id.jpg"
-
-if "example_image_path" not in st.session_state:
-    st.session_state.example_image_path = example_image_path
-
-if st.button("Upload Example Id"):
-    st.session_state.upload_occurrence = True
-    image = Image.open(example_image_path)
-    if "image" not in st.session_state:
-        st.session_state.image = image
-    temp_path = "temp_image.jpg"
-    image.save(temp_path)
-    st.image(st.session_state.image, caption="Uploaded Image", use_column_width=True)
-    parsed_data = parse_with_reg_expressions(st.session_state.extracted_text)
-    if "df" not in st.session_state:
-        st.session_state.df = pd.DataFrame(list(parsed_data.items()), columns=['Field', 'Value'])
-    os.remove(temp_path)
+    if "extracted_text" not in st.session_state:
+        st.session_state.extracted_text = extracted_text
+    if "upload_occurrence" not in st.session_state:
+        st.session_state.upload_occurrence = False
+    if "extraction_occurrence" not in st.session_state:
+        st.session_state.extraction_occurrence = False
+    if "data_form_occurrence" not in st.session_state:
+        st.session_state.data_form_occurrence = False
     
-if st.session_state.extraction_occurrence == False and st.session_state.upload_occurrence == True:
-    with st.form("extract_form"):
+    def data_form_callback():
+        st.session_state.data_form_occurrence = True
         
-        extract = st.form_submit_button("Extract", on_click=extraction_callback)
+    def extraction_callback():
+        st.session_state.extraction_occurrence = True
+    
+    def final_callback():
+        st.session_state.data_form_occurrence = False
+        st.session_state.extraction_occurrence = False
         
-if st.session_state.extraction_occurrence == True and st.session_state.data_form_occurrence == False:
+    def parse_with_reg_expressions(text):
+        data = {}
+        name_match = re.search(r"MOTORIST\.\s*(?P<name>[A-Z\s,\.]+)\s*SAMPLE", text)
+        if name_match:
+            data["Full Name"] = name_match.group("name").replace(",", "").strip()
+        address_match = re.search(r"(?P<address>\d+\s[A-Z\s]+)\s*,\s*NY\s*\d+", text)
+        if address_match:
+            data["Address"] = address_match.group("address").strip()
+        sex_match = re.search(r"Sex\s*(?P<sex>[MF])", text)
+        if sex_match:
+            data["Sex"] = sex_match.group("sex")
+        height_match = re.search(r"Height\s*(?P<height>\d+'-\d+\")", text)
+        if height_match:
+            data["Height"] = height_match.group("height")
+        dob_match = re.search(r"(?P<dob>\d{2}/\d{2}/\d{4})", text)
+        if dob_match:
+            data["DOB"] = dob_match.group("dob")
+        exp_match = re.search(r"Exes\s*(?P<exp>\d{2}/\d{2}/\d{4})", text)
+        if exp_match:
+            data["Expiration Date"] = exp_match.group("exp")
     
-    with st.form("data_form"):
-        st.data_editor(st.session_state.df,  hide_index = True, )
-        csv = st.session_state.df.to_csv(index=False)
-        if "csv" not in st.session_state:
-            st.session_state.csv = csv
-
-        st.submit_button = st.form_submit_button("Confirm and Submit", on_click=data_form_callback)
+        issue_match = re.search(r"issued\s*(?P<issue>\d{2}/\d{2}/\d{4})", text)
+        if issue_match:
+            data["Issue Date"] = issue_match.group("issue")
     
-if st.session_state.extraction_occurrence == True and st.session_state.data_form_occurrence == True:
-    st.markdown("""
-                ## Thank You For Submitting Your Id Info
-                """)
-    st.markdown("""
-                ### Download A Copy For Your Records Below\n
-                """)
-    st.download_button(
-        label = "Download CSV File and Reset Demo",
-        data=st.session_state.csv,
-        file_name = "example excel file / restart demo",
-        mime="text/csv", key="download_button",
-        on_click=final_callback()
-    )
+        return data
+    
+    st.title("ID Info Extraction Demo")
+    
+    example_image_path = "Image-to-Text-Scraper-Demo/example_id.jpg"
+    
+    if "example_image_path" not in st.session_state:
+        st.session_state.example_image_path = example_image_path
+    
+    if st.button("Upload Example Id"):
+        st.session_state.upload_occurrence = True
+        image = Image.open(example_image_path)
+        if "image" not in st.session_state:
+            st.session_state.image = image
+        temp_path = "temp_image.jpg"
+        image.save(temp_path)
+        st.image(st.session_state.image, caption="Uploaded Image", use_column_width=True)
+        parsed_data = parse_with_reg_expressions(st.session_state.extracted_text)
+        if "df" not in st.session_state:
+            st.session_state.df = pd.DataFrame(list(parsed_data.items()), columns=['Field', 'Value'])
+        os.remove(temp_path)
+        
+    if st.session_state.extraction_occurrence == False and st.session_state.upload_occurrence == True:
+        with st.form("extract_form"):
+            
+            extract = st.form_submit_button("Extract", on_click=extraction_callback)
+            
+    if st.session_state.extraction_occurrence == True and st.session_state.data_form_occurrence == False:
+        
+        with st.form("data_form"):
+            st.data_editor(st.session_state.df,  hide_index = True, )
+            csv = st.session_state.df.to_csv(index=False)
+            if "csv" not in st.session_state:
+                st.session_state.csv = csv
+    
+            st.submit_button = st.form_submit_button("Confirm and Submit", on_click=data_form_callback)
+        
+    if st.session_state.extraction_occurrence == True and st.session_state.data_form_occurrence == True:
+        st.markdown("""
+                    ## Thank You For Submitting Your Id Info
+                    """)
+        st.markdown("""
+                    ### Download A Copy For Your Records Below\n
+                    """)
+        st.download_button(
+            label = "Download CSV File and Reset Demo",
+            data=st.session_state.csv,
+            file_name = "example excel file / restart demo",
+            mime="text/csv", key="download_button",
+            on_click=final_callback()
+        )
